cfe1039ead5aaebc00be8222d3e620fe90a83c6d
Ismaheel <oladimeji2312@gmail.com>
2025-01-05 10:48:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Contact from './components/contact'
+const App = () => {
+  return (
+    <div>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import "./Contact.css/"
+const Contact = () => {
+    return (
+        <div className="container">
+            <h1>Contact Us</h1>
+            <form action="https://formsubmit.co/oladimeji2312@gmail.com" method="POST">
+                <input type="text" name="name" required placeholder="Your name" />
+                <input type="text" name="email" required placeholder="Your email" />
+                <input type="hidden" name="_autoresponse" value="Thanks for your inquiry, we will get back to you shortly."></input>
+                <textarea name="msg" placeholder="Your message"></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        
+    )
+}
+
+export default Contact;
