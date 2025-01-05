9836608b837ddd8bd62943c0eec8ceb77a8811be
Ismaheel <oladimeji2312@gmail.com>
2025-01-05 10:58:11 +0100

    New commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
             <form action="https://formsubmit.co/oladimeji2312@gmail.com" method="POST">
                 <input type="text" name="name" required placeholder="Your name" />
                 <input type="text" name="email" required placeholder="Your email" />
-                <input type="hidden" name="_autoresponse" value="Thanks for your inquiry, we will get back to you shortly."></input>
-                <textarea name="msg" placeholder="Your message"></textarea>
+                <input type="hidden" name="_autoresponse" value="Thank you for your inquiry, we will get back to you shortly."></input>
+                <textarea name="msg" placeholder="Type your message"></textarea>
                 <button type="submit">Send</button>
             </form>
         </div>
